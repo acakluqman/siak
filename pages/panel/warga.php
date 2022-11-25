650371e9553c09aecf82bc3abde779099a7f95aa
Luqman Hakim <luqmanul@its.ac.id>
2022-11-25 22:10:48 +0700

    filter warga

--- pages/panel/warga.php
+++ pages/panel/warga.php
@@ -3,12 +3,50 @@
 aksesOnly([2, 3, 4]);
 
 // data warga
-$warga = $conn->prepare("SELECT * FROM (SELECT w.nik, w.kartu_keluarga, w.nama, w.jk, w.tmp_lahir, w.tgl_lahir, p.nama AS pekerjaan FROM warga w
+$filter = "";
+if (!empty($_POST['usia'])) {
+    if ($_POST['usia'] == 'balita') {
+        $filter .= " AND umur < 5 ";
+    }
+    if ($_POST['usia'] == 'anak') {
+        $filter .= " AND umur BETWEEN 5 AND 12 ";
+    }
+    if ($_POST['usia'] == 'remaja') {
+        $filter .= " AND umur BETWEEN 12 AND 25 ";
+    }
+    if ($_POST['usia'] == 'dewasa') {
+        $filter .= " AND umur BETWEEN 26 AND 45 ";
+    }
+    if ($_POST['usia'] == 'pralansia') {
+        $filter .= " AND umur BETWEEN 45 AND 55 ";
+    }
+    if ($_POST['usia'] == 'lansia') {
+        $filter .= " AND umur > 55 ";
+    }
+}
+if (!empty($_POST['kawin'])) {
+    $filter .= " AND id_status_kawin = " . $_POST['kawin'] . " ";
+}
+if (!empty($_POST['hubungan'])) {
+    $filter .= " AND id_status_hubungan = " . $_POST['hubungan'] . " ";
+}
+
+$warga = $conn->prepare("SELECT * FROM (SELECT w.nik, w.kartu_keluarga, w.nama, w.jk, w.tmp_lahir, w.tgl_lahir, timestampdiff(year, w.tgl_lahir, curdate()) as umur, w.id_status_kawin, w.id_status_hubungan, p.nama AS pekerjaan
+    FROM warga w
     LEFT JOIN ref_pekerjaan p ON p.id_pekerjaan = w.id_pekerjaan
     WHERE NOT EXISTS (SELECT nik FROM rwt_kematian k WHERE k.nik = w.nik)) AS w1
     WHERE NOT EXISTS (SELECT * FROM rwt_mutasi m WHERE m.nik = w1.nik)
+    $filter
     ORDER BY w1.nama ASC");
 $warga->execute();
+
+// data status kawin
+$statusKawin = $conn->prepare("SELECT * FROM ref_status_kawin");
+$statusKawin->execute();
+
+// data status hubungan
+$statusHubungan = $conn->prepare("SELECT * FROM ref_status_hubungan");
+$statusHubungan->execute();
 
 // proses hapus warga
 if (isset($_POST['delete'])) {
@@ -40,11 +78,49 @@ if (isset($_POST['delete'])) {
     <div class="row">
         <div class="col-12">
             <div class="card">
+                <?php if ($_SESSION['level'] == 4) : ?>
+                    <div class="card-header">
+                        <a href="app.php?page=tambah_warga" class="btn btn-primary"><span class="fa fa-user-plus"></span> Tambah Warga</a>
+                    </div>
+                <?php endif ?>
                 <form action="<?= htmlspecialchars($_SERVER["PHP_SELF"] . '?page=warga') ?>" class="form" method="post">
                     <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <label for="usia" class="control-label">Kelompok Usia</label>
+                                <select name="usia" id="usia" class="form-control select2">
+                                    <option value="">Semua Kelompok Usia</option>
+                                    <option value="balita" <?= (isset($_POST['usia']) && $_POST['usia'] == 'balita') ? 'selected' : '' ?>>Balita (0-5 Tahun)</option>
+                                    <option value="anak" <?= (isset($_POST['usia']) && $_POST['usia'] == 'anak') ? 'selected' : '' ?>>Anak-Anak (5-11 Tahun)</option>
+                                    <option value="remaja" <?= (isset($_POST['usia']) && $_POST['usia'] == 'remaja') ? 'selected' : '' ?>>Remaja (12-25 Tahun)</option>
+                                    <option value="dewasa" <?= (isset($_POST['usia']) && $_POST['usia'] == 'dewasa') ? 'selected' : '' ?>>Dewasa (26-45 Tahun)</option>
+                                    <option value="pralansia" <?= (isset($_POST['usia']) && $_POST['usia'] == 'pralansia') ? 'selected' : '' ?>>Pra-Lansia (45-55 Tahun)</option>
+                                    <option value="lansia" <?= (isset($_POST['usia']) && $_POST['usia'] == 'lansia') ? 'selected' : '' ?>>Lansia (>55 Tahun)</option>
+                                </select>
+                            </div>
+                            <div class="col-md-3">
+                                <label for="kawin" class="control-label">Status Kawin</label>
+                                <select name="kawin" id="kawin" class="form-control select2">
+                                    <option value="">Semua Status Kawin</option>
+                                    <?php foreach ($statusKawin->fetchAll() as $row) { ?>
+                                        <option value="<?= $row['id_status_kawin'] ?>" <?= (isset($_POST['kawin']) && $_POST['kawin'] == $row['id_status_kawin']) ? 'selected' : '' ?>><?= $row['nama'] ?></option>
+                                    <?php } ?>
+                                </select>
+                            </div>
+                            <div class="col-md-3">
+                                <label for="hubungan" class="control-label">Status Hubungan</label>
+                                <select name="hubungan" id="hubungan" class="form-control select2">
+                                    <option value="">Semua Status Hubungan</option>
+                                    <?php foreach ($statusHubungan->fetchAll() as $row) { ?>
+                                        <option value="<?= $row['id_status_hubungan'] ?>" <?= (isset($_POST['hubungan']) && $_POST['hubungan'] == $row['id_status_hubungan']) ? 'selected' : '' ?>><?= $row['nama'] ?></option>
+                                    <?php } ?>
+                                </select>
+                            </div>
+                        </div>
                     </div>
                     <div class="card-footer">
-                        <button type="submit" name="filter" class="btn btn-success"><i class="fa fa-search" aria-hidden="true"></i> Filter</button>
+                        <button type="submit" name="filter" class="btn btn-primary"><i class="fa fa-search" aria-hidden="true"></i> Filter</button>
+                        <!-- <button type="submit" name="export" class="btn btn-success"><i class="fa fa-file-excel" aria-hidden="true"></i> Export Excel</button> -->
                     </div>
                 </form>
             </div>
@@ -52,11 +128,6 @@ if (isset($_POST['delete'])) {
 
         <div class="col-12">
             <div class="card">
-                <?php if ($_SESSION['level'] == 4) : ?>
-                    <div class="card-header">
-                        <a href="app.php?page=tambah_warga" class="btn btn-primary"><span class="fa fa-user-plus"></span> Tambah</a>
-                    </div>
-                <?php endif ?>
                 <div class="card-body table-responsive">
                     <table class="table table-striped table-hover w-100" aria-describedby="warga" id="warga">
                         <thead>
